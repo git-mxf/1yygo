66c070052b1e13446997ab9c99eb1b2931169faf
Dave Meng <75994345@qq.com>
2017-07-17 09:18:19 +0800

    added footer from mxf

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,9 @@
 import React,{Component} from "react";
 import "./index.scss"
+import {
+	NavLink
+} from "react-router-dom"
+
 
 class App extends Component {
 	constructor(props) {
@@ -18,6 +22,40 @@ class App extends Component {
 					{this.props.children}
 				</section>
 
+				<footer>
+					<ul>
+						<li>
+							<NavLink to="/CloudShop" activeClassName="myselected">
+								<span></span>
+								云购
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/New" activeClassName="myselected">
+								<span></span>
+								最新揭晓
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/Shai" activeClassName="myselected">
+								<span></span>
+								晒单
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/Cart" activeClassName="myselected">
+								<span></span>
+								购物车
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/MyCloud" activeClassName="myselected">
+								<span></span>
+								我的云购
+							</NavLink>
+						</li>
+					</ul>
+				</footer>
 			</div>
 			)
 	}
