40ff4df6e0d1c7bcf47896b376ad077c9405d11d
lijing <lj55810@163.com>
2017-07-17 19:35:54 +0800

    ont-lijing

--- src/components/MyCloud/index.js
+++ src/components/MyCloud/index.js
@@ -1,6 +1,7 @@
 import React,{Component} from "react";
 import "./index.scss"
 
+
 class MyCloud extends Component {
 	constructor(props) {
 		super(props);
@@ -9,8 +10,38 @@ class MyCloud extends Component {
 
 	render(){
 		return (
-			<div>
-				MyCloud
+			<div id="box">
+				<div className="head">
+					<img src="../src/assets/img/head.jpg" className="userface"/>
+					<div className="news">
+						<p className="person">USER.1016978536 <span className="number">(ID:1016978536)</span ></p>
+						<div className="person-small"><span className="star"></span> 云购小将</div>
+					</div>
+					<div className="set"></div>
+				</div>
+				<div className="top-up">
+					<div className="fortune">
+					<a href="javascript:;"><span className="money">20</span><span className="usable">可用福分</span></a>
+					</div>
+					<div className="rental">
+					<a href="javascript:;"><span className="money">￥0.00</span><span className="usable">账户总额</span></a>
+						
+					</div>
+					<div className="pay">
+						<a href="javascript:;"className="gopay" >去充值</a>
+					</div>
+				</div>
+				
+				<div className="myrecord">
+				<a href="javascript:;" className="lists"><span className="jilu"></span>我的云购记录<span className="you" ></span></a>
+					<a href="javascript:;" className="lists"><span className="goods"></span>获得的商品<span className="you"></span></a>
+					<a href="javascript:;" className="lists"><span className="show"></span>我的晒单<span className="you"></span></a>
+					<a href="javascript:;" className="lists"><span className="purse"></span>我的钱包<span className="you"></span></a>
+					<a href="javascript:;" className="lists-help"><span className="help"></span>帮助与反馈<span className="you"></span></a>
+					<p className="tel">客服热线：4000-588-688  (工作时间9:00-21:00)</p>
+				</div>
+
+				
 			</div>
 			)
 	}
